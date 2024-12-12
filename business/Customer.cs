a6eadbc5ea716bb83069259b266556341fc42827
User <User@DESKTOP-5PR9RUE>
2024-12-12 09:15:11 +0200

    Add project files.

---
+++ business/Customer.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace business
+{
+    public class Customer
+    {
+        private int id;
+        private string name;
+        private bool clubMember;
+        private int kamutIskaot;
+        private int totalIskaot;
+        public Customer(int id, string name) 
+        {
+            this.id = id;
+            this.name = name;
+            this.clubMember = false;
+            this.totalIskaot = 0;
+            this.kamutIskaot = 0;
+        }
+        public void MakeIska(int amount)
+        {
+            this.kamutIskaot++;
+            if (!clubMember)
+            {
+                this.totalIskaot += amount;
+            }
+            else
+            {
+                this.totalIskaot += (amount * 9)/10;
+            }
+            if ((this.kamutIskaot >= 10) || (this.totalIskaot > 1000))
+            {
+                clubMember = true;
+            }
+        }
+        public bool IsBetterCustomer(Customer other)
+        {
+            return this.totalIskaot > other.totalIskaot;
+        }
+        public bool GetClubMember()
+        {
+            return this.clubMember;
+        }
+
+    }
+}
---
+++ business/Program.cs
@@ -0,0 +1,22 @@
+﻿using business;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+    public static int AmountMembers(Customer[] customers)
+    {
+        int result = 0;
+        for (int i = 0; i < customers.Length; i++)
+        {
+            bool clubStatus = customers[i].GetClubMember();
+            if (clubStatus) 
+            {
+                result++;
+            }
+        }
+        return result;
+    }
+}
