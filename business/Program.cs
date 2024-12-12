a04993839bbda0268c2bc18898420434e312f175
mark <markyaholon@gmail.com>
2024-12-12 09:43:02 +0200

    main

--- business/Program.cs
+++ business/Program.cs
@@ -4,7 +4,21 @@ internal class Program
 {//mark 12/12
     private static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        Customer c1 = new Customer(1, "mark");
+        c1.MakeIska(1200);
+        Console.WriteLine(c1);
+        Customer c2 = new Customer(2, "Lior");
+        Console.WriteLine(c2);
+        if (c1.IsBetterCustomer(c2))
+        {
+            Console.WriteLine("mark better then lior");
+        }
+        else
+        {
+            Console.WriteLine("lior is better");
+        }
+        Customer[] arr= new Customer[] { c1, c2 };
+        Console.WriteLine(AmountMembers(arr));
     }
     public static int AmountMembers(Customer[] customers)
     {// get list and returns how many club members
